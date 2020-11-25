a8fe28b045f252ca6d6e7123c3611656de3dd84e
Aliu Yusuf <aliu.yusuf@outlook.com>
2020-11-25 19:34:30 +0100

    Using autoload for classes

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -3,10 +3,7 @@
 $app =[];
 
 $app['config']= require 'config.php';
-require 'core/Router.php';
-require 'core/Database/Connection.php';
-require 'core/Request.php';
-require 'core/Database/QueryBuilder.php';
+
 
 
 
--- routes.php
+++ routes.php
@@ -4,7 +4,7 @@
 $router->get('','controllers/index.php');
 $router->get('about','controllers/about.php');
 $router->get('contact','controllers/contact.php');
-$router->get('about/culture','controllers/about/culture.php');
+$router->get('about/culture','controllers/about-culture.php');
 $router->post('names','controllers/add-name.php');
 
 
